2489dbb3173fcde39339f2c97ad0f27793788e16
tyemai <tyeers@gmail.com>
2018-03-31 22:28:26 -0700

    solved problem 2

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
+//1.
+/*
 var warmup = new Promise(function (resolve, reject){
-
   resolve('hello promise, yay i resolved')
-
   reject('oops i did it again you got rejected')
 })
 
@@ -15,24 +14,94 @@ warmup.then(function(result){
   console.log(err)
 }
 })
+*/
 
+//2.
 
-warmup.then(function(result){
-  delay(1000, warmup)
-})
+var delay = function(milliseconds) {
+  var promise = new Promise(function (resolve, reject) {
+   setTimeout(function(){
+      resolve(milliseconds)
+      console.log(promise)
+   }, milliseconds)
+  })
+  console.log(promise)
+  return promise
+}
 
-function delay(milliseconds,name){
-    //var warmup2 = new Promise(function (resolve, reject){
-    //  resolve('yay i resolved')
-    //  reject('oops i did it again')
-  //})
+/*
+function countDown(milliseconds) {
+  if (milliseconds > 0) {
+    delay(milliseconds)
+    console.log(milliseconds)
+    countDown(milliseconds - 100)
+  } else {
+    console.log('done!')
+  }
+}
 
-    name.then(function(result){
+function countDown2(){
+  if (milliseconds > 0) {
+    console.log(milliseconds)
+    return (milliseconds - 100)
+  }else {
+    console.log("done!")
+  }
+}
+*/
+var countDown = function(resolveFromDelay) {
+  if (resolveFromDelay > 1) {
+    console.log(resolveFromDelay)
+    var promise = new Promise(function(resolve,reject){
       setTimeout(function(){
-        console.log(result)
-      }, milliseconds),
-      function(err) {
-        console.log(err)
-      }
+        //console.log(resolveFromDelay - 100)
+        resolve(resolveFromDelay  - 100)
+      }, 500)
     })
- }
+
+  } else if(resolveFromDelay === 0) {
+    console.log('done!')
+  }
+    return promise
+}
+delay(1000).then(countDown).then(countDown).then(countDown).then(countDown).then(countDown).then(countDown).then(countDown).then(countDown).then(countDown).then(countDown).then(countDown)
+
+/*
+var firstMethod = function() {
+   var promise = new Promise(function(resolve, reject){
+      setTimeout(function() {
+         console.log('first method completed');
+         resolve({data: '123'});
+      }, 2000);
+   });
+   return promise;
+};
+
+
+var secondMethod = function(someStuff) {
+  console.log(someStuff)
+   var promise = new Promise(function(resolve, reject){
+      setTimeout(function() {
+         console.log('second method completed');
+         resolve({newData: someStuff.data + ' some more data'});
+      }, 2000);
+   });
+   return promise;
+};
+
+var thirdMethod = function(someStuff) {
+   var promise = new Promise(function(resolve, reject){
+      setTimeout(function() {
+         console.log('third method completed');
+         resolve({result: someStuff.newData});
+      }, 3000);
+   });
+   return promise;
+};
+
+firstMethod()
+   .then(secondMethod)
+   .then(thirdMethod).then(function(result){
+     console.log(result)
+   })
+*/
